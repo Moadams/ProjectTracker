c326b71f1a1275231981ab7cbf90374d7662641f
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-04 15:58:59 +0000

    create task and project statuses

---
+++ src/main/java/com/buildmaster/projecttracker/enums/ProjectStatus.java
@@ -0,0 +1,5 @@
+package com.buildmaster.projecttracker.enums;
+
+public enum ProjectStatus {
+    ACTIVE, COMPLETED, ON_HOLD, CANCELLED
+}
---
+++ src/main/java/com/buildmaster/projecttracker/enums/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.buildmaster.projecttracker.enums;
+
+public enum TaskStatus {
+    TODO, IN_PROGRESS, IN_REVIEW, COMPLETED, BLOCKED
+}
