5217a7cc3505e57ec18647bee5c681c21024aba0
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-05 21:31:20 +0000

    add finding by actor name, entitytype and actor name

--- src/main/java/com/buildmaster/projecttracker/repository/AuditLogRepository.java
+++ src/main/java/com/buildmaster/projecttracker/repository/AuditLogRepository.java
@@ -1,10 +1,18 @@
 package com.buildmaster.projecttracker.repository;
 
+import com.buildmaster.projecttracker.enums.EntityType;
 import com.buildmaster.projecttracker.model.AuditLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface AuditLogRepository extends MongoRepository<AuditLog, String> {
+
+    List<AuditLog> findByEntityType(EntityType entityType);
+    List<AuditLog> findByActorName(String actorName);
+    List<AuditLog> findByEntityTypeAndActorName(EntityType entityType, String actorName);
 
 }
