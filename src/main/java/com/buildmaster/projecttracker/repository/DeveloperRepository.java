750cbff11db60b963c45e1dfec2260b5b8bb1688
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-05 15:15:01 +0000

    override find all to return pagination

--- src/main/java/com/buildmaster/projecttracker/repository/DeveloperRepository.java
+++ src/main/java/com/buildmaster/projecttracker/repository/DeveloperRepository.java
@@ -1,9 +1,13 @@
 package com.buildmaster.projecttracker.repository;
 
 import com.buildmaster.projecttracker.model.Developer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+    @Override
+    Page<Developer> findAll(Pageable pageable);
 }
