69a5b7524deccadb917ab4e94ee266b5edf6551c
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-06 10:29:27 +0000

    Make model cacheable

--- src/main/java/com/buildmaster/projecttracker/model/Task.java
+++ src/main/java/com/buildmaster/projecttracker/model/Task.java
@@ -9,8 +9,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.Cache;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,6 +24,8 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@EntityListeners(AuditingEntityListener.class)
 public class Task {
 
     @Id
