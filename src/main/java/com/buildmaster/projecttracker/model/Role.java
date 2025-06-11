be40d962960ab94796879e0c02666fb2d7dc7190
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-11 10:19:40 +0000

    add roles model

---
+++ src/main/java/com/buildmaster/projecttracker/model/Role.java
@@ -0,0 +1,26 @@
+package com.buildmaster.projecttracker.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
