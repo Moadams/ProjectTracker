780bece00bae69250564466f6496521557f1e0ef
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-11 10:54:06 +0000

    make user repository a repository recognizable by spring

--- src/main/java/com/buildmaster/projecttracker/repository/UserRepository.java
+++ src/main/java/com/buildmaster/projecttracker/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.buildmaster.projecttracker.repository;
 
 import com.buildmaster.projecttracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
