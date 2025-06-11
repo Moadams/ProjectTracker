21964d29c32422032fc66993db2fc76033a41bdb
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-11 10:55:18 +0000

    add role repository to get role by name

---
+++ src/main/java/com/buildmaster/projecttracker/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.buildmaster.projecttracker.repository;
+
+import com.buildmaster.projecttracker.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
