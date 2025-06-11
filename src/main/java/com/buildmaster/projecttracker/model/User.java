471c7ec12ece86a2419a43363f93bb161d87da1c
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-11 10:15:02 +0000

    add user model

---
+++ src/main/java/com/buildmaster/projecttracker/model/User.java
@@ -0,0 +1,47 @@
+package com.buildmaster.projecttracker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users", uniqueConstraints = @UniqueConstraint(name = "unique_user_email", columnNames = "email"))
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Enter a valid email address")
+    @NotBlank(message = "Email must not be blank")
+    @Column(unique=true, nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Column(nullable = false)
+    private String password;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
