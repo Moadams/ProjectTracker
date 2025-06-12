b5b97c56458e47b5d0c976090a01ebe2dbd22729
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-12 21:35:29 +0000

    handle security exceptions

--- src/main/java/com/buildmaster/projecttracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/buildmaster/projecttracker/exception/GlobalExceptionHandler.java
@@ -3,11 +3,15 @@ package com.buildmaster.projecttracker.exception;
 import com.buildmaster.projecttracker.dto.CustomApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +24,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(CustomApiResponse.error(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomApiResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -29,6 +32,24 @@ public class GlobalExceptionHandler {
 
         CustomApiResponse<Map<String, String>> response = CustomApiResponse.error("Validation failed", errors);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<CustomApiResponse> handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+        CustomApiResponse errorDetails = new CustomApiResponse(false, "Access Denied: You do not have permission to access this resource.", request.getDescription(false),LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<CustomApiResponse> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
+        CustomApiResponse errorDetails = new CustomApiResponse(false, "Authentication Failed: " + ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<CustomApiResponse> handleResponseStatusException(ResponseStatusException ex, WebRequest request) {
+        CustomApiResponse errorDetails = new CustomApiResponse(false, ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
