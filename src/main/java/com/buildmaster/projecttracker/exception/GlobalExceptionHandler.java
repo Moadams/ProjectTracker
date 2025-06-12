c19a8b2e06938668a7e4fd7339022fd31f02d491
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-12 23:31:38 +0000

    handle usernotfound exception

--- src/main/java/com/buildmaster/projecttracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/buildmaster/projecttracker/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.buildmaster.projecttracker.exception;
 
 import com.buildmaster.projecttracker.dto.CustomApiResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -43,6 +45,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<CustomApiResponse> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
         CustomApiResponse errorDetails = new CustomApiResponse(false, "Authentication Failed: " + ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<CustomApiResponse> handleUserNotFoundException(UsernameNotFoundException ex, WebRequest request) {
+        CustomApiResponse errorDetails = new CustomApiResponse(false, "User not found: " + ex.getMessage(), request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<CustomApiResponse> handleExpiredJwtException(ExpiredJwtException ex, WebRequest request) {
+        CustomApiResponse errorDetails = new CustomApiResponse(false, "Token has expired: " + ex.getMessage(), request.getDescription(false), LocalDateTime.now());
         return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
