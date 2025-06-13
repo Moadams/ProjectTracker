c40f7fac8cd8a49b311b679bc202cdc7f041bed7
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-06-13 20:44:59 +0000

    refactor: restrict ROLE_CONTRACTOR to other endpoints apart from projects

--- src/main/java/com/buildmaster/projecttracker/config/SecurityConfig.java
+++ src/main/java/com/buildmaster/projecttracker/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -74,7 +75,10 @@ public class SecurityConfig {
 
                     authorize.requestMatchers("/api/v1/admin/**").hasRole("ADMIN");
 
-                    authorize.requestMatchers("/api/v1/**").authenticated();
+                    authorize.requestMatchers("/api/v1/projects/**").authenticated();
+                    authorize.requestMatchers("/api/v1/tasks/**").hasAnyRole("ADMIN","DEVELOPER", "MANAGER");
+                    authorize.requestMatchers("/api/v1/developers/**").hasAnyRole("ADMIN","DEVELOPER","MANAGER");
+                    authorize.requestMatchers("/api/v1/notifications/**").authenticated();
                 })
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
